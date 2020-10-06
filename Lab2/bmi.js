ae67be74fd238d22bb66212e5bca6fe0eae217c9
Sinead199 <sineadk98@gmail.com>
2020-10-06 17:39:45 +0100

    Lab2 - complete

---
+++ Lab2/bmi.js
@@ -0,0 +1,18 @@
+//blueprint of an object
+class BMI { 
+   //Starting point of the class - Initiallised 
+   constructor(height, weight){ 
+        this.height = height;
+        this.weight = weight;
+    }
+
+    calculateBMI(){
+        //BMI calculation
+        let bmi = this.weight/(this.height**2);
+        return bmi;
+    }
+}
+
+let MyBmi = new BMI(185, 95);
+let calculateBMI = MyBmi.calculateBMI();
+console.log(calculateBMI);
